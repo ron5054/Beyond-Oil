f927b3a43fc839c286700d254c1f23d67a3b35da
Yaron Levy <ron5054@gmail.com>
2025-03-30 14:06:21 +0300

    Config fix

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,8 @@ const nextConfig = {
         hostname: '**',
       },
     ],
+    unoptimized: false,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   experimental: {
     serverActions: {
